812d3bba1dbe0cef71ecd83264cf196e429fe758
Rabiul37 <hmrabiul37@gmail.com>
2023-10-20 08:39:28 +0600

    data geting from the data base

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const productCollection = client.db("productDB").collection("product");
+    app.get("/products/", async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     app.post("/products", async (req, res) => {
       const product = req.body;
