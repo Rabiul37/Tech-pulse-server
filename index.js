daffa022b63a42d942a3dbfff2c6ee8285220786
Rabiul37 <hmrabiul37@gmail.com>
2023-10-20 08:46:50 +0600

    data show in the client site is completed

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
 
     app.post("/products", async (req, res) => {
       const product = req.body;
